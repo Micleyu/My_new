daf5d7ce2a22e74cdd7b45ef47b75581fa0775be
Administrator <1635872541@qq.com>
2018-07-27 16:36:41 +0800

    vue conmonents

---
+++ vue/弹出层.js
@@ -0,0 +1,9 @@
+<div class="dialog">
+    <div class="title">
+        <span>{{title}}</span>
+        <span class="close">X</span>
+    </div>
+    <div class="content">
+        <slot></slot>
+    </div>
+</div>
